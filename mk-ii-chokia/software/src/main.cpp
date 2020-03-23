4ac2d5d7b6417b4f71b96c84df4eb931854bdb94
Myke <entozoon@gmail.com>
2020-03-23 18:49:20 +0000

    Ganking together libs for chonky serial display

--- mk-ii-chokia/software/src/main.cpp
+++ mk-ii-chokia/software/src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include <SPI.h>
+#include <Wire.h>
 
-void setup() {
-  // put your setup code here, to run once:
+U8G2_ST7920_128X64_1_SW_SPI u8g2(
+    U8G2_R0, // rotation
+    PA1,     // Clock "E"
+    PA2,     // Data "R/W"
+    PA3,     // CS "RS"
+    PA4      // Reset "RST"
+);
+// Also connect +5v to VCC, BLA
+// and - to GND, PSB, BLK
+
+void setup(void)
+{
+  u8g2.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void)
+{
+  u8g2.firstPage();
+  do
+  {
+    u8g2.setFont(u8g2_font_ncenB14_tr);
+    u8g2.drawStr(0, 24, "Hello World!");
+  } while (u8g2.nextPage());
 }
