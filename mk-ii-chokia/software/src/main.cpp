17dc83edf2358032a5c8ec16fa8a375afc6e5645
Myke <entozoon@gmail.com>
2020-03-23 17:12:04 +0000

    Re-organising for Chokia mk II

---
+++ mk-ii-chokia/software/src/main.cpp
@@ -0,0 +1,236 @@
+#include <Arduino.h>
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <Fonts/FreeSans9pt7b.h>
+#include <Fonts/Tiny3x3a2pt7b.h>
+#include <Fonts/Org_01.h>
+#include <Fonts/Picopixel.h>
+#include <Fonts/TomThumb.h>
+#include <nokia-myke.h>
+
+// // BLINK
+// void setup()
+// {
+// pinMode(LED_BUILTIN, OUTPUT);
+// }
+// void loop()
+// {
+//   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+//   delay(200);                      // wait for a second
+//   digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+//   delay(200);                      // wait for a second
+// }
+
+// // SEND
+// #include <SPI.h>
+// #include <LoRa.h>
+// int counter = 0;
+// void setup()
+// {
+//   delay(4000); // for uploading to device
+//   Serial.begin(115200);
+//   Serial.println("\nLoRa Sender");
+// //          (nss, reset, dio0); (dio0 unused)
+// //          (D8, D0);
+// LoRa.setPins(15, 16);
+//   if (!LoRa.begin(433E6))  // 433 is legal in the UK and capable by device. 863-870 otherwise. worried it'll clash though
+//   {
+//     Serial.println("Starting LoRa failed!");
+//     while (1)
+//       ;
+//   }
+// }
+// void loop()
+// {
+//   Serial.print("Sending packet: ");
+//   Serial.println(counter);
+//   // send packet
+//   LoRa.beginPacket();
+//   LoRa.print("hello ");
+//   LoRa.print(counter);
+//   LoRa.endPacket();
+//   counter++;
+//   delay(1000);
+// }
+
+// // RECEIVE
+// #include <SPI.h>
+// #include <LoRa.h>
+// void setup()
+// {
+//   delay(4000); // for uploading to device
+//   Serial.begin(115200);
+//   Serial.println("\nLoRa Receiver");
+//   //          (nss, reset, dio0); (dio0 unused)
+//   //          (D8, D0);
+//   // LoRa.setPins(15, 16);
+//   //          (D8, D0);
+//   LoRa.setPins(10, 9);
+//   if (!LoRa.begin(433E6)) // 433 is legal in the UK and capable by device. 863-870 otherwise. worried it'll clash though
+//   {
+//     Serial.println("Starting LoRa failed!");
+//     while (1)
+//       ;
+//   }
+// }
+// void loop()
+// {
+//   // try to parse packet
+//   int packetSize = LoRa.parsePacket();
+//   if (packetSize)
+//   {
+//     // received a packet
+//     Serial.print("Received packet '");
+//     // read packet
+//     while (LoRa.available())
+//     {
+//       Serial.print((char)LoRa.read());
+//     }
+//     // print RSSI of packet
+//     Serial.print("' with RSSI "); // Received Signal Strength Indicator (0 is theoretical full strength)
+//     Serial.println(LoRa.packetRssi());
+//   }
+// }
+
+// // TEST VOLTMETER READ
+// void setup()
+// {
+//   Serial.begin(115200);
+//   delay(4000);
+// }
+// void loop()
+// {
+//   int val = analogRead(A0);
+//   Serial.println(val);
+//   delay(100);
+// }
+
+// // SENDY RECEIVY NANO
+// #include <SPI.h>
+// #include <LoRa.h>
+// long counter = 0;
+// long ticker = 0;
+// long tickerSnapshot = 0;
+// void setup()
+// {
+//   delay(4000); // for uploading to device
+//   Serial.begin(115200);
+//   Serial.println("\nLoRa Receiver");
+//   //          (nss, reset, dio0); (dio0 unused)
+//   // LoRa.setPins(15, 16); // Wemos (D8, D0)
+//   LoRa.setPins(10, 9);    //  Nano (D8, D0) // IM NOT CONVINCED THESE ARE RIGHT (even though it works). Think it might be a 1:1 relationship with gpio
+//   if (!LoRa.begin(433E6)) // 433 is legal in the UK and capable by device. 863-870 otherwise. worried it'll clash though
+//   {
+//     Serial.println("Starting LoRa failed!");
+//     while (1)
+//       ;
+//   }
+// }
+// void loop()
+// {
+//   // try to parse packet
+//   int packetSize = LoRa.parsePacket();
+//   if (packetSize)
+//   {
+//     // received a packet
+//     Serial.print("Received packet '");
+//     // read packet
+//     while (LoRa.available())
+//     {
+//       Serial.print((char)LoRa.read());
+//     }
+//     // print RSSI of packet
+//     Serial.print("' with RSSI "); // Received Signal Strength Indicator (0 is theoretical full strength)
+//     Serial.println(LoRa.packetRssi());
+//   }
+
+//   if (ticker > 5000)
+//   {
+//     tickerSnapshot = millis();
+//     Serial.print("Sending packet: ");
+//     Serial.println(counter);
+//     // send packet
+//     LoRa.beginPacket();
+//     LoRa.print("hello ");
+//     LoRa.print(counter);
+//     LoRa.endPacket();
+//     counter++;
+//   }
+//   ticker = millis() - tickerSnapshot;
+// }
+
+// LCD Test
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+//                                         (clk, din, dc, ce, rst)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 2);
+// Hardware SPI (faster, but must use certain hardware pins):
+// SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
+// MOSI is LCD DIN - this is pin 11 on an Arduino Uno
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+// Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+// Note with hardware SPI MISO and SS pins aren't used but will still be read
+// and written to during SPI transfer.  Be careful sharing these pins!
+void setup()
+{
+  delay(5000);
+  display.begin();
+  display.setContrast(60);
+  display.setRotation(2);
+  display.setFont(&__nokiafc224pt7b);
+  display.setTextColor(BLACK);
+  display.setTextSize(1);
+}
+void loop()
+{
+  display.clearDisplay();
+  display.setCursor(0, 10);
+  display.println("3");
+  display.println("messages");
+  display.println("received");
+  display.display();
+
+  delay(20000);
+}
+
+// // Backlight dimming test
+// // Not quite sure about this.. I heard a 39ohm resistor to gpio could control it. dimming on would be ace
+// // but - don't stress too much, if it's just on all the tiem that'd be okay too
+// // https://www.algissalys.com/electronics/nokia-5110-lcd-backlight-to-ground-using-gpio
+// void setup()
+// {
+//   Serial.begin(115200);
+//   delay(5000);
+//   // I think D1, D2, are 1, 2.. A0, A1 are A0, A1..
+//   pinMode(9, OUTPUT);
+// }
+// float x = 0;
+// float pi = 3.141592;
+// void loop()
+// {
+//   // if (x > 255)
+//   //   x = 0;
+//   // Serial.print(x);
+//   // Serial.print(' ');
+//   // analogWrite(9, x);
+//   // delay(20);
+//   // x++;
+
+//   if (x > 4 * pi)
+//     x = 0;
+
+//   int y = floor(255 * ((sin(x / 2 - pi / 2)) / 2 + .5));
+//   // Serial.print(y);
+//   // Serial.print(' ');
+//   analogWrite(9, y);
+//   x += 0.01;
+//   delay(5);
+// }
